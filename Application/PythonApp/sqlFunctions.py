00bfeb2a4eccecfefaacfbe04abed01d8f3fb512
mavcook <mavcook@umich.edu>
2016-11-04 20:17:54 -0400

    SQLite3 working
    Former-commit-id: d4e21d86766791ce9099e8c04f95fd52dca3adab

--- Application/PythonApp/sqlFunctions.py
+++ Application/PythonApp/sqlFunctions.py
@@ -1,16 +1,18 @@
 from flask import *
-#import sqlite3
-from extensions import db
-
-#conn = sqlite3.connect('database.db', check_same_thread=False)
-#cursor = conn.cursor()
-cursor = db.cursor()
+import sqlite3
+#from extensions import db
+	
+conn = sqlite3.connect('database.db', check_same_thread=False)
+conn.row_factory = sqlite3.Row
+cursor = conn.cursor()
+#cursor = db.cursor()
 
 
 # Effects: Runs a MySQL query that returns all images in the database
 def getImages():
 	cursor.execute("SELECT * FROM images ORDER BY imgId DESC")
 	results = cursor.fetchall()
+	print "\n\nResults: ", results
 	return results
 
 # Effects: Runs a MySQL query that returns a specific image in the database
@@ -25,6 +27,7 @@ def getImageData(imgId):
 def insertImageToDB(format, imgId):
 	cursor.execute("INSERT INTO images (format, imgId) VALUES (\'" \
 		+ format + "\', \'" + imgId + "\')")
+	conn.commit()
 	if (cursor.fetchall()):
 		return True
 	else:
